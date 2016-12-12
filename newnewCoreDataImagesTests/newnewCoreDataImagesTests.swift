1966d418ce5aad194c8f55aed455919e4fee1a24
Cesar <cesar@stevens-macbook-pro.local>
2016-12-12 09:05:34 -0800

    Initial Commit

---
+++ newnewCoreDataImagesTests/newnewCoreDataImagesTests.swift
@@ -0,0 +1,36 @@
+//
+//  newnewCoreDataImagesTests.swift
+//  newnewCoreDataImagesTests
+//
+//  Created by Cesar on 12/12/16.
+//  Copyright © 2016 Cesar. All rights reserved.
+//
+
+import XCTest
+@testable import newnewCoreDataImages
+
+class newnewCoreDataImagesTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
